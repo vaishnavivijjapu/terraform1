12dc3a9d94376e0f11bc115d6556e0447e47f2c2
vaishnavivijjapu <136182907+vaishnavivijjapu@users.noreply.github.com>
2023-11-07 21:45:41 +0530

    Update Ec2-Instance-with-Securitygroup.tf

--- Ec2-Instance-with-Securitygroup.tf
+++ Ec2-Instance-with-Securitygroup.tf
@@ -1,32 +1,34 @@
 provider "aws" {
   region     = "ap-south-1"
+  access_key = "AKIAYC4Q3JISASCB44B5"
+  secret_key = "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"
 }
 
 #security group
 resource "aws_security_group" "web_access" {
-        name = "web_access"
-        description = "allow ssh and http"
+  name        = "web_access"
+  description = "allow ssh and http"
 
-        ingress {
-                from_port = 80
-                to_port = 80
-                protocol = "tcp"
-                cidr_blocks = ["0.0.0.0/0"]
-        }
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-        ingress {
-                from_port = 22
-                to_port = 22
-                protocol = "tcp"
-                cidr_blocks = ["0.0.0.0/0"]
-        }
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-        egress {
-                from_port = 0
-                to_port = 0
-                protocol = "-1"
-                cidr_blocks = ["0.0.0.0/0"]
-        }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
 
 }
@@ -34,17 +36,19 @@ resource "aws_security_group" "web_access" {
 
 
 resource "aws_instance" "web-server" {
-  ami           = "ami-0447a12f28fddb066"
+  ami               = "ami-0a6ed6689998f32a5"
   availability_zone = "ap-south-1a"
-  instance_type = "t2.micro"
-  security_groups = ["${aws_security_group.web_access.name}"]
+  instance_type     = "t2.micro"
+  security_groups   = ["${aws_security_group.web_access.name}"]
   tags = {
-    Name  = "hello-World"
-    Stage = "testing"
+    Name     = "hello-World"
+    Stage    = "testing"
     Location = "INDIA"
   }
 
 }
+
+                               
 
 
 /*
