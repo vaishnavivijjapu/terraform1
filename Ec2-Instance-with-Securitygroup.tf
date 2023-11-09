b430844e8a7389a45fe69f1ed8d4378def715b8f
vaishnavivijjapu <136182907+vaishnavivijjapu@users.noreply.github.com>
2023-11-09 12:30:11 +0530

    Update Ec2-Instance-with-Securitygroup.tf

--- Ec2-Instance-with-Securitygroup.tf
+++ Ec2-Instance-with-Securitygroup.tf
@@ -1,61 +1,12 @@
 provider "aws" {
   region     = "ap-south-1"
-
 }
-
-#security group
-resource "aws_security_group" "web_access" {
-  name        = "web_access"
-  description = "allow ssh and http"
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-
-}
-#security group end here
-
-
-resource "aws_instance" "web-server" {
-  ami               = "ami-02e94b011299ef128"
-  availability_zone = "ap-south-1"
-  instance_type     = "t2.micro"
-  security_groups   = ["${aws_security_group.web_access.name}"]
+ 
+ 
+resource "aws_instance" "Test-Server" {
+  ami           = "ami-02e94b011299ef128"
+  instance_type = "t2.micro"
   tags = {
-    Name     = "hello-World"
-    Stage    = "testing"
-    Location = "INDIA"
-  }
-
+Name = "Test-Server"
 }
-
-                               
-
-
-/*
-run
-terraform validate
-terraform plan
-terraform apply
-check ec2 instance on aws dashboard
-then destroy using
-terraform destroy
-*/
+}
